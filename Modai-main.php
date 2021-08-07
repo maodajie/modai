61fa0c22bae99ed7f7ce60755d8cbb2c648a9a0f
毛大姐 <maodajie.com@gmail.com>
2021-08-07 16:20:10 +0800

    修复定时任务参数错误的问题

---
+++ Modai-main.php
@@ -0,0 +1,24 @@
+<?php
+/*!
+ * @author:大橙子
+ * @website:https://maodajie.com
+ * @telegram:https://t.me/maccms_tool
+ * @qqcode:1570457334
+ */
+
+namespace addons\modai;
+
+use think\Addons;
+
+class Modai extends Addons
+{
+    public function install()
+    {
+        return true;
+    }
+
+    public function uninstall()
+    {
+        return true;
+    }
+}
